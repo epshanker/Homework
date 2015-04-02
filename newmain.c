17b483507ca26b5b5222d6812f26be63a2c037e2
epshanker <EmilyShanker2016@u.northwestern.edu>
2015-04-02 13:12:57 -0500

    Homework1- MPLABX project

---
+++ newmain.c
@@ -0,0 +1,132 @@
+/* 
+ * File:   newmain.c
+ * Author: epshanker
+ *
+ * Created on March 31, 2015, 10:57 PM
+ */
+
+#include<xc.h> // processor SFR definitions
+#include<sys/attribs.h> // __ISR macro
+
+// DEVCFG0
+    #pragma config DEBUG = OFF // no debugging
+    #pragma config JTAGEN = OFF // no jtag
+    #pragma config ICESEL = ICS_PGx1 // use PGED1 and PGEC1
+    #pragma config PWP = OFF // no write protect
+    #pragma config BWP = OFF // not boot write protect
+    #pragma config CP = OFF // no code protect
+
+// DEVCFG1
+    #pragma config FNOSC = PRIPLL // use primary oscillator with pll
+    #pragma config FSOSCEN = OFF // turn off secondary oscillator
+    #pragma config IESO = OFF // no switching clocks
+    #pragma config POSCMOD = HS // high speed crystal mode
+    #pragma config OSCIOFNC = OFF // free up secondary osc pins
+    #pragma config FPBDIV = DIV_1 // divide CPU freq by 1 for bus clock
+    #pragma config FCKSM = CSDCMD // do not enable clock switch
+    #pragma config WDTPS = PS1048576 // slowest wdt
+    #pragma config WINDIS = OFF // no wdt window
+    #pragma config FWDTEN = OFF // wdt off by default
+    #pragma config FWDTWINSZ = WINSZ_25 // wdt window at 25%
+
+// DEVCFG2 - get the CPU clock to 40MHz
+    #pragma config FPLLIDIV = DIV_2 // divide input clock to be in range 4-5MHz
+    #pragma config FPLLMUL = MUL_20 // multiply clock after FPLLIDIV
+    #pragma config FPLLODIV = DIV_2 // divide clock after FPLLMUL to get 40MHz
+    #pragma config UPLLIDIV = DIV_2 // divide 8MHz input clock
+    #pragma config UPLLEN = ON // USB clock on
+
+// DEVCFG3
+    #pragma config USERID = 0 // some 16bit userid
+    #pragma config PMDL1WAY = ON // not multiple reconfiguration, check this
+    #pragma config IOL1WAY = ON // not multimple reconfiguration, check this
+    #pragma config FUSBIDIO = ON // USB pins controlled by USB module
+    #pragma config FVBUSONIO = ON // controlled by USB module
+
+int readADC(void);
+
+int main() {
+    // startup
+    __builtin_disable_interrupts();
+
+    // set the CP0 CONFIG register to indicate that
+    // kseg0 is cacheable (0x3) or uncacheable (0x2)
+    // see Chapter 2 "CPU for Devices with M4K Core"
+    // of the PIC32 reference manual
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+
+    // no cache on this chip!
+
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+
+    // disable JTAG to be able to use TDI, TDO, TCK, TMS as digital
+    DDPCONbits.JTAGEN = 0;
+
+    __builtin_enable_interrupts();
+    
+    // set up USER pin as input
+    ANSELBbits.ANSB13 = 0; // sets pin B13 to digital input
+
+    // set up LED1 as digital output
+    TRISBbits.TRISB7 = 0;
+    //RPB7Rbits.RPB7R = 0b0001; // set B7 to U1TX
+    
+    // Set LED2 as OC1 using Timer2 at 1kHz
+    RPB15Rbits.RPB15R = 0b0101; // sets pin B15 to output compare
+    T2CONbits.TCKPS = 0;		// sets pre-scaler to 1
+	PR2 = 39999;				// sets maximum counter value
+	TMR2 = 0;					// initializes timer 2
+	OC1CONbits.OCM = 0b110;		// use without fail safe
+	OC1RS = 2000;				// sets duty cycle to 50%
+	OC1R = 2000;				// sets initial duty cycle to 50%
+	T2CONbits.ON = 1;			// turns timer on
+	OC1CONbits.ON = 1;			// turns output control on
+
+    // Read the analog input
+    ANSELAbits.ANSA0 = 1; // sets pin A0 to analog input
+    AD1CON3bits.ADCS = 3;
+    AD1CHSbits.CH0SA = 0;
+    AD1CON1bits.ADON = 1;
+    
+    while (1) {
+        _CP0_SET_COUNT(0); // set core timer to 0
+        LATBINV = 0b10000000; // invert pin B7 for LED1
+
+        // wait for half a second, setting LED brightness to pot angle
+        while (_CP0_GET_COUNT() < 10000000) {
+            int val;
+            val = readADC();
+            OC1RS = val * 39; // value/1023 * 400000 simplifies to about 39
+
+            if (PORTBbits.RB13 == 1) {
+                // nothing
+            } else {
+                LATBINV = 0b10000000; // invert LED1 as fast as possible
+            }
+        }
+    }
+    
+
+}
+
+int readADC(void) {
+    int elapsed = 0;
+    int finishtime = 0;
+    int sampletime = 20;
+    int a = 0;
+
+    AD1CON1bits.SAMP = 1;
+    elapsed = _CP0_GET_COUNT();
+    finishtime = elapsed + sampletime;
+    while (_CP0_GET_COUNT() < finishtime) {
+    }
+    AD1CON1bits.SAMP = 0;
+    while (!AD1CON1bits.DONE) {
+    }
+    a = ADC1BUF0;
+    return a;
+}
